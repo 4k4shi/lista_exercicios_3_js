ec76a01f886a47556c64f438e51f7883ed2be9dc
4k4shi <4k4shisj@gmail.com>
2019-12-11 22:49:18 -0300

    Resolução da segunda questão

--- dois/js/script.js
+++ dois/js/script.js
@@ -1,23 +1,23 @@
-console.log("#Calcular a média de diversas notas");
+console.log("#Trazer o maior número de notas");
 
-let nota = 0, notas = 0, contador = 0, media =0, cfm = false;
+let n1,n2,n3,n4,n5,maior;
 
-do{
-	nota = prompt("Digite um número");
-	if(nota === "fim"){
-		cfm = confirm("Encerrando o cálculo de médias ...");
-		if(cfm == false){
-			nota = 0;
-		}
-	}else{
-		contador++;
-		notas += Number(nota); 
-		media = notas/contador; 
-		if(media > 0 || media <= 0){
-			alert("Média:"+media);	
-		}
-		
-	}
+n1 = prompt("Digite um número");
+n2 = prompt("Digite outro número");
+n3 = prompt("Digite outro número");
+n4 = prompt("Digite outro número");
+n5 = prompt("Digite outro número");
+
+if(n1 > n2 && n1 > n3 && n1 > n4 && n1 > n5){
+	maior = n1;
+}else if(n2 > n1 && n2 > n3 && n2 > n4 && n2 > n5){
+	maior = n2;
+}else if(n3 > n4 && n3 > n1 && n3 > n2 && n3 > n5){
+	maior = n3;
+}else if(n4 > n5 && n4 > n3 && n4 > n1 && n4 > n5){
+	maior = n4;
+}else{
+	maior = n5;
 }
-while(nota != "fim");
-console.log("obg ...");
+
+alert("Maior: "+n5);
