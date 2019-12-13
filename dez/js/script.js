ca67c9a0f980a0e2ca9ae236027ab4baa899c345
4k4shi <4k4shisj@gmail.com>
2019-12-13 17:33:11 -0300

    Resolução da décima e última \0\  /0/   \0/

---
+++ dez/js/script.js
@@ -0,0 +1,13 @@
+console.log("#Saudação");
+
+const dataAtual = new Date();
+const horaAtual = dataAtual.getHours();
+if(horaAtual >= 0 && horaAtual < 6){
+	alert("Boa madrugada ( ͡° ͜ʖ ͡°)  \nSão exatemente " + horaAtual + " horas");
+}else if(horaAtual >= 6 && horaAtual < 12){
+	alert("Bom dia :) \nSão exatemente " + horaAtual + " horas");
+}else if(horaAtual >= 12 && horaAtual < 6){
+	alert("Boa tarde :D \nSão exatemente " + horaAtual + " horas");
+}else{
+	alert("Boa noite ;D \nSão exatemente " + horaAtual + " horas");
+}
