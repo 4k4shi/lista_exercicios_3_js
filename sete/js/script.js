fe48d7ac32c036e1ec2071fe1c49ccc64e58537d
4k4shi <4k4shisj@gmail.com>
2019-12-13 15:55:40 -0300

    Resolução da sétima

---
+++ sete/js/script.js
@@ -0,0 +1,11 @@
+console.log("#Solicitação do nome");
+
+let nome,nomeSemEspaco,cont;
+
+nome = prompt("Digite seu nome completo >-<");
+
+nomeSemEspaco = nome.replace(" ","");
+
+alert("QUANTIDADE-> Nome com espaço: "+nome.length+" caracters"
+	+
+	" /Nomes sem caracters: "+nomeSemEspaco.length+" caracteres");
